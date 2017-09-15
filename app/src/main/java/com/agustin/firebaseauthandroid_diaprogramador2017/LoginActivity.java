2f570ea90b02d0d2ffaef36962324f8fbd38948e
agustinCaire <agustincaire@gmail.com>
2017-09-15 11:01:21 -0300

    Forzar Seleccionar cuenta google

--- app/src/main/java/com/agustin/firebaseauthandroid_diaprogramador2017/LoginActivity.java
+++ app/src/main/java/com/agustin/firebaseauthandroid_diaprogramador2017/LoginActivity.java
@@ -21,6 +21,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -174,6 +175,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     /***********************************************************/
 
     private void loginWithGoogle() {
+        mGoogleApiClient.clearDefaultAccountAndReconnect();
+
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
